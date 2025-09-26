2fbfc13b59d002397f6d18319c9d118684e131c8
Dennis Mureti <dennismureti47@gmail.com>
2025-09-26 17:19:55 +0300

    remove conn_health_checks

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -7,7 +7,6 @@ from pathlib import Path
 from decouple import config 
 import dj_database_url
 
-
 # Load environment variables
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
@@ -79,7 +78,6 @@ DATABASES = {
     'default': dj_database_url.config(
         default=DATABASE_URL,
         conn_max_age=600,
-        conn_health_checks=True,
     )
 }
 
